2a800fa3c78aef6569c4b5c06112ba83b8280fdd
Sarah Luthra <saluthra@big01.seas.upenn.edu>
2023-04-05 16:58:25 -0400

    copied in the pipeline for lab 5 but will not compile lol

--- lab5-superscalar/lc4_cla.v
+++ lab5-superscalar/lc4_cla.v
@@ -1,4 +1,5 @@
-/* TODO: INSERT NAME AND PENNKEY HERE */
+/* Sarah Luthra, saluthra
+   Rashmi Iyer, rashmii */
 
 `timescale 1ns / 1ps
 `default_nettype none
@@ -15,6 +16,23 @@ module gp1(input wire a, b,
    assign p = a | b;
 endmodule
 
+
+/*module gp2(input wire [1:0] gin, 
+           input wire [1:0] pin,
+           input wire cin,
+           output wire gout,
+           output wire pout,
+           output wire [2:0] cout);
+
+    wire gentemp1;
+    wire proptemp1;
+    wire gentemp2;
+    wire proptemp2;
+    gp1 g1(.a(gin[0]), .b(pin[0]), .g(gentemp1), .p(proptemp1));
+    gp1 g2(.a(gin[1]), .b(pin[1]), .g(gentemp2), .p(proptemp2));
+
+endmodule*/
+
 /**
  * Computes aggregate generate/propagate signals over a 4-bit window.
  * @param gin incoming generate signals 
@@ -24,11 +42,21 @@ endmodule
  * @param pout whether these 4 bits collectively would propagate an incoming carry (ignoring cin)
  * @param cout the carry outs for the low-order 3 bits
  */
-module gp4(input wire [3:0] gin, pin,
+module gp4(input wire [3:0] gin, 
+           input wire [3:0] pin,
            input wire cin,
-           output wire gout, pout,
+           output wire gout,
+           output wire pout,
            output wire [2:0] cout);
-   
+
+    assign cout[0] = gin[0] | (pin[0] & cin); // c1 
+    assign cout[1] = gin[1] | (pin[1] & gin[0]) | (pin[1] & pin[0] & cin); //c2
+    assign cout[2] = gin[2] | (pin[2] & gin[1]) | (pin[2] & pin[1] & gin[0]) | (pin[2] & pin[1] & pin[0] & cin); //c3
+   // assign cout[3] = gin[3] | (pin[3] & gin[2]) | (pin[3] & pin[2] & gin[1]) | (pin[3] & pin[2] & pin[1] & gin[0]) | (pin[3] & pin[2] & pin[1] & pin[0] & cin[0]); //c4
+
+    assign pout = (& pin);
+    assign gout = gin[3] | (pin[3] & gin[2]) | (pin[3] & pin[2] & gin[1]) | (pin[3] & pin[2] & pin[1] & gin[0]);
+    
 endmodule
 
 /**
@@ -43,4 +71,54 @@ module cla16
    input wire         cin,
    output wire [15:0] sum);
 
+  wire [15:0] ginit;
+  wire [15:0] pinit;
+  wire [15:0] cbus;
+  wire gout40;
+  wire pout40;
+  wire gout41;
+  wire pout41;
+  wire gout42;
+  wire pout42;
+  wire gout43;
+  wire pout43;
+
+  //ginit and pinit generation
+  genvar i;
+  for (i = 0; i < 16; i = i+1) begin
+    gp1 g(.a(a[i]), .b(b[i]), .g(ginit[i]), .p(pinit[i]));
+  end
+
+  //carry generation
+  assign cbus[0] = cin;
+  gp4 gp40(.gin(ginit[3:0]), .pin(pinit[3:0]), .cin(cin), .gout(gout40), .pout(pout40), .cout(cbus[3:1]));
+  assign cbus[4] = gout40 | (pout40 & cin);
+  gp4 gp41(.gin(ginit[7:4]), .pin(pinit[7:4]), .cin(cbus[4]), .gout(gout41), .pout(pout41), .cout(cbus[7:5]));
+  assign cbus[8] = (cin & pout40 & pout41) | (gout40 & pout41) | gout41;
+  gp4 gp42(.gin(ginit[11:8]), .pin(pinit[11:8]), .cin(cbus[8]), .gout(gout42), .pout(pout42), .cout(cbus[11:9]));
+  assign cbus[12] = (cin & pout40 & pout41 & pout42) | (gout40 & pout41 & pout42) | (gout41 & pout42) | gout42;
+  gp4 gp43(.gin(ginit[15:12]), .pin(pinit[15:12]), .cin(cbus[12]), .gout(gout43), .pout(pout43), .cout(cbus[15:13]));
+
+  //assign cbus[15] = (cin & pout40 & pout41 & pout42 & pout43) | (gout40 & pout41 & pout42 & pout43) | (gout41 & pout42 & pout43) | (gout42 & pout43) | gout43;
+
+  genvar j;
+  for (j = 0; j < 16; j = j + 1) begin
+    assign sum[j] = (a[j] ^ b[j]) ^ cbus[j];
+  end
+  
 endmodule
+
+
+/** Lab 2 Extra Credit, see details at
+  https://github.com/upenn-acg/cis501/blob/master/lab2-alu/lab2-cla.md#extra-credit
+ If you are not doing the extra credit, you should leave this module empty.
+ */
+module gpn
+  #(parameter N = 4)
+  (input wire [N-1:0] gin, pin,
+   input wire  cin,
+   output wire gout, pout,
+   output wire [N-2:0] cout);
+ 
+endmodule
+
--- lab5-superscalar/lc4_superscalar.v
+++ lab5-superscalar/lc4_superscalar.v
@@ -48,7 +48,146 @@ module lc4_processor(input wire         clk,             // main clock
 
    /***  YOUR CODE HERE ***/
 
+   assign led_data = switch_data;
 
+   wire [15:0] pc_plus_one;
+   cla16 pc_cla(.a(pc), .b(16'b1), .cin(1'b0), .sum(pc_plus_one));
+
+   wire [15:0] pc;
+   wire [15:0] next_pc;
+   assign next_pc = take_branch ? x_aluout : (load_to_use_stall ? pc : pc_plus_one);
+
+   Nbit_reg #(16, 16'h8200) f_pc_reg (.in(next_pc), .out(pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+
+   // code to fetch here
+   wire [15:0] d_pc;
+   wire [15:0] d_insn;
+   wire [15:0] d_pc_plus_one;
+   Nbit_reg #(16, 16'h8200) d_pc_reg (.in(take_branch ? 16'b0 : pc), .out(d_pc), .clk(clk), .we(!load_to_use_stall), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) d_insn_reg (.in(take_branch ? 16'b0 : i_cur_insn), .out(d_insn), .clk(clk), .we(!load_to_use_stall), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) d_pc_plus_one_reg (.in(take_branch ? 16'b0 : pc_plus_one), .out(d_pc_plus_one), .clk(clk), .we(!load_to_use_stall), .gwe(gwe), .rst(rst));
+
+
+   //code to decode here
+   wire [17:0] d_decode_output;
+   wire [15:0] d_r1val_output;
+   wire [15:0] d_r2val_output;
+   wire [15:0] x_pc;
+   wire [15:0] x_insn;
+   wire [17:0] x_decodevals;
+   wire [15:0] x_r1val;
+   wire [15:0] x_r2val;
+   wire [15:0] x_pc_plus_one;
+   wire load_to_use_stall = x_decodevals[14] & ((d_decode_output[3] & (d_decode_output[2:0] == x_decodevals[10:8])) | 
+                           (d_decode_output[7] & d_decode_output[6:4] == x_decodevals[10:8] & !d_decode_output[15]) |
+                           d_decode_output[16]);
+
+   lc4_decoder decoder(.insn(d_insn), .r1sel(d_decode_output[2:0]), .r1re(d_decode_output[3]), .r2sel(d_decode_output[6:4]), .r2re(d_decode_output[7]), 
+      .wsel(d_decode_output[10:8]), .regfile_we(d_decode_output[11]), .nzp_we(d_decode_output[12]), .select_pc_plus_one(d_decode_output[13]),
+      .is_load(d_decode_output[14]), .is_store(d_decode_output[15]), .is_branch(d_decode_output[16]), .is_control_insn(d_decode_output[17]));
+
+   //update i_rd, i_wdata, i_rd_we based on writeback
+   lc4_regfile#(16) regfile (.clk(clk), .gwe(gwe), .rst(rst), .i_rs(d_decode_output[2:0]), .o_rs_data(d_r1val_output), 
+      .i_rt(d_decode_output[6:4]), .o_rt_data(d_r2val_output), .i_rd(w_decodevals[10:8]), .i_wdata(w_rd_write_val), .i_rd_we(w_decodevals[11]));
+
+   wire [15:0] d_actual_r1val;
+   wire [15:0] d_actual_r2val;
+   assign d_actual_r1val = (d_decode_output[3] & w_decodevals[10:8] == d_decode_output[2:0] & w_decodevals[11]) ?
+                           w_rd_write_val : d_r1val_output;
+   assign d_actual_r2val = (d_decode_output[7] & w_decodevals[10:8] == d_decode_output[6:4] & w_decodevals[11]) ?
+                           w_rd_write_val : d_r2val_output;
+
+   Nbit_reg #(16, 16'h8200) x_pc_reg (.in((load_to_use_stall | take_branch) ? 16'b0 : d_pc), .out(x_pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) x_insn_reg (.in(take_branch ? 16'b0 : (load_to_use_stall ? 16'b1 : d_insn)), .out(x_insn), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(18, 18'b0) x_decodevals_reg (.in((take_branch | load_to_use_stall) ? 18'b0 : d_decode_output), .out(x_decodevals), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) x_r1val_reg (.in((take_branch | load_to_use_stall) ? 16'b0 : d_actual_r1val), .out(x_r1val), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) x_r2val_reg (.in((take_branch | load_to_use_stall) ? 16'b0 : d_actual_r2val), .out(x_r2val), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) x_pc_plus_one_reg (.in((take_branch | load_to_use_stall) ? 16'b0 : d_pc_plus_one), .out(x_pc_plus_one), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+
+   //code to execute here
+
+   wire [15:0] x_aluout;
+   wire [15:0] m_pc;
+   wire [15:0] m_insn;
+   wire [17:0] m_decodevals;
+   wire [15:0] m_r1val;
+   wire [15:0] m_r2val;
+   wire [15:0] m_aluout;
+   wire [15:0] x_actual_r1val;
+   wire [15:0] x_actual_r2val;
+   wire [15:0] m_pc_plus_one;
+   assign x_actual_r1val = (x_decodevals[3] & !m_decodevals[14] & m_decodevals[10:8] == x_decodevals[2:0] & m_decodevals[11]) ?
+                           m_aluout : ((x_decodevals[3] & w_decodevals[10:8] == x_decodevals[2:0] & w_decodevals[11]) ?
+                           w_rd_write_val : x_r1val);
+   assign x_actual_r2val = (x_decodevals[7] & !m_decodevals[14] & m_decodevals[10:8] == x_decodevals[6:4] & m_decodevals[11]) ?
+                           m_aluout : ((x_decodevals[7] & w_decodevals[10:8] == x_decodevals[6:4] & w_decodevals[11]) ?
+                           w_rd_write_val : x_r2val);
+
+
+   lc4_alu alu(.i_insn(x_insn), .i_pc(x_pc), .i_r1data(x_actual_r1val), .i_r2data(x_actual_r2val), .o_result(x_aluout));
+
+   wire take_branch;
+   assign take_branch = (x_decodevals[16] & ((x_insn[11:9] & (m_decodevals[12] ? nzp_towrite: m_nzp_out)) != 3'b0)) | x_decodevals[17];
+
+   Nbit_reg #(16, 16'h8200) m_pc_reg (.in(x_pc), .out(m_pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) m_insn_reg (.in(x_insn), .out(m_insn), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(18, 18'b0) m_decodevals_reg (.in(x_decodevals), .out(m_decodevals), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) m_r1val_reg (.in(x_actual_r1val), .out(m_r1val), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) m_r2val_reg (.in(x_actual_r2val), .out(m_r2val), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) m_aluout_reg (.in(x_aluout), .out(m_aluout), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) m_pc_plus_one_reg (.in(x_pc_plus_one), .out(m_pc_plus_one), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+
+   //code to memory here 
+
+   assign o_dmem_addr = (m_decodevals[14] | m_decodevals[15]) ? m_aluout : 16'b0;
+   assign o_dmem_we = m_decodevals[15];
+   assign o_dmem_towrite = (m_decodevals[15] & w_decodevals[11] & m_decodevals[6:4] == w_decodevals[10:8]) ?
+                           w_rd_write_val : m_r2val;
+
+   wire[2:0] nzp_towrite;
+   wire[2:0] m_nzp_out;
+   Nbit_reg #(3, 3'b0) nzp_reg (.in(nzp_towrite), .out(m_nzp_out), .clk(clk), .we(m_decodevals[12]), .gwe(gwe), .rst(rst));
+
+   assign nzp_towrite[2] = $signed(m_rd_write_val) < 0;
+   assign nzp_towrite[1] = $signed(m_rd_write_val) == 0;
+   assign nzp_towrite[0] = $signed(m_rd_write_val) > 0;
+
+   wire[15:0] m_rd_write_val;
+   assign m_rd_write_val = m_decodevals[13] ? m_pc_plus_one : (m_decodevals[14] ? i_cur_dmem_data : m_aluout);
+
+   assign test_nzp_new_bits  = m_nzp_out;
+
+   wire [15:0] w_pc;
+   wire [15:0] w_insn;
+   wire [17:0] w_decodevals;
+   wire [15:0] w_rd_write_val;
+   wire [2:0] w_nzp_out;
+   wire [15:0] w_dmem_data;
+   wire [15:0] w_dmem_to_write;
+   wire [15:0] w_dmem_addr;
+
+   Nbit_reg #(16, 16'h8200) w_pc_reg (.in(m_pc), .out(w_pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) w_insn_reg (.in(m_insn), .out(w_insn), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(18, 18'b0) w_decodevals_reg (.in(m_decodevals), .out(w_decodevals), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) w_rd_writeval_reg (.in(m_rd_write_val), .out(w_rd_write_val), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(3, 3'b0) w_nzp_out_reg (.in(m_nzp_out), .out(w_nzp_out), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) w_dmem_data_reg (.in(i_cur_dmem_data), .out(w_dmem_data), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) w_dmem_to_write_reg (.in(o_dmem_towrite), .out(w_dmem_to_write), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'b0) w_dmem_addr_reg (.in(o_dmem_addr), .out(w_dmem_addr), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+
+   //code for write here (done above with reg file)
+   
+   assign o_cur_pc = pc;
+   assign test_stall = (w_insn == 16'b0) ? 2'b10 : (w_insn == 16'b1 ? 2'b11 : 2'b00);
+   assign test_cur_pc = w_pc;
+   assign test_cur_insn = w_insn;
+   assign test_regfile_we = w_decodevals[11];
+   assign test_regfile_wsel = w_decodevals[10:8];
+   assign test_regfile_data = w_rd_write_val;
+   assign test_nzp_we = w_decodevals[12];
+   assign test_dmem_we = w_decodevals[15];
+   assign test_dmem_addr = w_dmem_addr;
+   assign test_dmem_data = w_decodevals[14] ? w_dmem_data : (w_decodevals[15] ? w_dmem_to_write : 16'b0);
 
 
 
