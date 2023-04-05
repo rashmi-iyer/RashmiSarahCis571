9dea6a1a592df53ddedda9495da53ac573ede8e3
Sarah Luthra <saluthra@big01.seas.upenn.edu>
2023-04-05 16:28:30 -0400

    lab 5 regfile done

--- lab5-regfile/lc4_regfile_ss.v
+++ lab5-regfile/lc4_regfile_ss.v
@@ -41,4 +41,88 @@ module lc4_regfile_ss #(parameter n = 16)
 
    /*** TODO: Your Code Here ***/
 
+   wire [n-1:0] r0_out;
+   wire [n-1:0] r1_out;
+   wire [n-1:0] r2_out;
+   wire [n-1:0] r3_out;
+   wire [n-1:0] r4_out;
+   wire [n-1:0] r5_out;
+   wire [n-1:0] r6_out;
+   wire [n-1:0] r7_out;
+
+   Nbit_reg #(n, 0) reg0 (.in(i_rd_we_B & i_rd_B == 3'b000 ? i_wdata_B : i_wdata_A), .out(r0_out), .clk(clk), .we((i_rd_we_A & i_rd_A == 3'b000) | (i_rd_we_B & i_rd_B == 3'b000)), .gwe(gwe), .rst(rst));
+   Nbit_reg #(n, 0) reg1 (.in(i_rd_we_B & i_rd_B == 3'b001 ? i_wdata_B : i_wdata_A), .out(r1_out), .clk(clk), .we((i_rd_we_A & i_rd_A == 3'b001) | (i_rd_we_B & i_rd_B == 3'b001)), .gwe(gwe), .rst(rst));
+   Nbit_reg #(n, 0) reg2 (.in(i_rd_we_B & i_rd_B == 3'b010 ? i_wdata_B : i_wdata_A), .out(r2_out), .clk(clk), .we((i_rd_we_A & i_rd_A == 3'b010) | (i_rd_we_B & i_rd_B == 3'b010)), .gwe(gwe), .rst(rst));
+   Nbit_reg #(n, 0) reg3 (.in(i_rd_we_B & i_rd_B == 3'b011 ? i_wdata_B : i_wdata_A), .out(r3_out), .clk(clk), .we((i_rd_we_A & i_rd_A == 3'b011) | (i_rd_we_B & i_rd_B == 3'b011)), .gwe(gwe), .rst(rst));
+   Nbit_reg #(n, 0) reg4 (.in(i_rd_we_B & i_rd_B == 3'b100 ? i_wdata_B : i_wdata_A), .out(r4_out), .clk(clk), .we((i_rd_we_A & i_rd_A == 3'b100) | (i_rd_we_B & i_rd_B == 3'b100)), .gwe(gwe), .rst(rst));
+   Nbit_reg #(n, 0) reg5 (.in(i_rd_we_B & i_rd_B == 3'b101 ? i_wdata_B : i_wdata_A), .out(r5_out), .clk(clk), .we((i_rd_we_A & i_rd_A == 3'b101) | (i_rd_we_B & i_rd_B == 3'b101)), .gwe(gwe), .rst(rst));
+   Nbit_reg #(n, 0) reg6 (.in(i_rd_we_B & i_rd_B == 3'b110 ? i_wdata_B : i_wdata_A), .out(r6_out), .clk(clk), .we((i_rd_we_A & i_rd_A == 3'b110) | (i_rd_we_B & i_rd_B == 3'b110)), .gwe(gwe), .rst(rst));
+   Nbit_reg #(n, 0) reg7 (.in(i_rd_we_B & i_rd_B == 3'b111 ? i_wdata_B : i_wdata_A), .out(r7_out), .clk(clk), .we((i_rd_we_A & i_rd_A == 3'b111) | (i_rd_we_B & i_rd_B == 3'b111)), .gwe(gwe), .rst(rst));
+
+   wire[15:0] o_rs_data_A_init;
+   assign o_rs_data_A = (i_rd_we_B & i_rd_B == i_rs_A) ? i_wdata_B : ((i_rd_we_A & i_rd_A == i_rs_A) ? i_wdata_A : o_rs_data_A_init);
+   wire[15:0] o_rt_data_A_init;
+   assign o_rt_data_A = (i_rd_we_B & i_rd_B == i_rt_A) ? i_wdata_B : ((i_rd_we_A & i_rd_A == i_rt_A) ? i_wdata_A : o_rt_data_A_init);
+
+   mux8to1 mux_rs_A(.S1(i_rs_A[2]), .S2(i_rs_A[1]), .S3(i_rs_A[0]), .A(r7_out), .B(r3_out), .C(r5_out), .D(r1_out), 
+                    .E(r6_out), .F(r2_out), .G(r4_out), .H(r0_out), .Out(o_rs_data_A_init));
+
+   mux8to1 mux_rt_A(.S1(i_rt_A[2]), .S2(i_rt_A[1]), .S3(i_rt_A[0]), .A(r7_out), .B(r3_out), .C(r5_out), .D(r1_out), 
+                    .E(r6_out), .F(r2_out), .G(r4_out), .H(r0_out), .Out(o_rt_data_A_init));
+
+
+   wire[15:0] o_rs_data_B_init;
+   assign o_rs_data_B = (i_rd_we_B & i_rd_B == i_rs_B) ? i_wdata_B : ((i_rd_we_A & i_rd_A == i_rs_B) ? i_wdata_A : o_rs_data_B_init);
+   wire[15:0] o_rt_data_B_init;
+   assign o_rt_data_B = (i_rd_we_B & i_rd_B == i_rt_B) ? i_wdata_B : ((i_rd_we_A & i_rd_A == i_rt_B) ? i_wdata_A : o_rt_data_B_init);
+
+   mux8to1 mux_rs_B(.S1(i_rs_B[2]), .S2(i_rs_B[1]), .S3(i_rs_B[0]), .A(r7_out), .B(r3_out), .C(r5_out), .D(r1_out), 
+                    .E(r6_out), .F(r2_out), .G(r4_out), .H(r0_out), .Out(o_rs_data_B_init));
+
+   mux8to1 mux_rt_B(.S1(i_rt_B[2]), .S2(i_rt_B[1]), .S3(i_rt_B[0]), .A(r7_out), .B(r3_out), .C(r5_out), .D(r1_out), 
+                    .E(r6_out), .F(r2_out), .G(r4_out), .H(r0_out), .Out(o_rt_data_B_init));
+
+
+endmodule
+
+
+
+module mux2to1(input wire S,
+            input wire [15:0] A,
+            input wire [15:0] B,
+            output wire [15:0] Out);
+      assign Out = S ? A : B;
+endmodule
+
+module mux4to1(input wire S1,
+            input wire S2,
+            input wire [15:0] A,
+            input wire [15:0] B,
+            input wire [15:0] C,
+            input wire [15:0] D,
+            output wire [15:0] Out);
+      wire [15:0] mux1out;
+      wire [15:0] mux2out;
+      mux2to1 m1(.S(S1), .A(A), .B(B), .Out(mux1out));
+      mux2to1 m2(.S(S1), .A(C), .B(D), .Out(mux2out));
+      mux2to1 m3(.S(S2), .A(mux1out), .B(mux2out), .Out(Out));
+endmodule
+
+module mux8to1(input wire S1,
+            input wire S2,
+            input wire S3,
+            input wire [15:0] A,
+            input wire [15:0] B,
+            input wire [15:0] C,
+            input wire [15:0] D,
+            input wire [15:0] E,
+            input wire [15:0] F,
+            input wire [15:0] G,
+            input wire [15:0] H,
+            output wire [15:0] Out);
+      wire [15:0] mux1out;
+      wire [15:0] mux2out;
+      mux4to1 m1(.S1(S1), .S2(S2), .A(A), .B(B), .C(C), .D(D), .Out(mux1out));
+      mux4to1 m2(.S1(S1), .S2(S2), .A(E), .B(F), .C(G), .D(H), .Out(mux2out));
+      mux2to1 m3(.S(S3), .A(mux1out), .B(mux2out), .Out(Out));
 endmodule
